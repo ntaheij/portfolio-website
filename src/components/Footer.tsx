64534b0651d7976847182d768904431e1ddd9849
ntaheij <noah@taheij.nl>
2023-06-13 13:43:09 +0200

    Changed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,12 +13,12 @@ const Footer = () => {
       {path !== "/contact" ?
           <section className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-7xl mx-auto px-4 xl:px-0 mb-4">
             <div className="flex flex-col gap-2 items-start px-16 py-16 lg:py-0 justify-center border border-stone-200 rounded-lg">
-              <h2>Lets get in touch 👉</h2>
+              <h2>Let's get in touch 👉</h2>
               <p className="mb-4">Interested in hiring or getting to know me? You can get in touch with me below.</p>
               <Button link={"/contact"} text={"Get in touch"} type={"secondary-white"} />
             </div>
             <div>
-              <Image className="rounded-lg" src={erasmusbrug} alt={"Placeholder for footer"} />
+              <Image className="rounded-lg" src={erasmusbrug} alt={"Erasmusbrug in footer"} />
             </div>
           </section>
         :
